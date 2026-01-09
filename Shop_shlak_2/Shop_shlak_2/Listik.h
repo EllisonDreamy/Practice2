fcafee0e5b75da24101b24d4ad69a1a3480e329c
EllisonDreamy <superdreamsboy@gmail.com>
2026-01-09 15:20:33 +0300

    listik and heroes

--- Shop_shlak_2/Shop_shlak_2/Listik.h
+++ Shop_shlak_2/Shop_shlak_2/Listik.h
@@ -1,7 +1,25 @@
 #pragma once
+#include "Clients.h"
+
+struct NodeList {
+    Client data;
+    NodeList* next;
+    NodeList* prev;
+    int number;
+
+    NodeList();
+};
 
 class Listik {
 private:
+    NodeList* firstNode;
+    NodeList* currentNode;
 public:
-	Listik();
+    Listik(Client firstNodeData);
+
+    void addClient(Client newData);
+
+    NodeList* getFirstClient() const;
+
+    void deleteFirstClient();
 };
